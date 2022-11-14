cf0fea7ebb5e28050616a8f283eea64d83fafacc
guillaume <>
2022-11-14 09:49:15 +0100

    1er commit

---
+++ articles.php
@@ -0,0 +1,14 @@
+<?php require __DIR__ . '/templates/header.php'; ?>
+<h1>Articles</h1>
+<p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla id
+    placerat odio. Aliquam in eros id sem ultricies pharetra at eu
+    tortor. Etiam viverra, erat vel imperdiet sollicitudin, dui
+    justo ornare libero, id blandit velit neque id arcu. Donec
+    consequat nisi vestibulum ultricies malesuada. Ut vehicula
+    consequat enim, quis accumsan massa luctus vel. In hac habitasse
+    platea dictumst. Mauris accumsan nibh mi, et condimentum enim
+    facilisis sed. Sed cursus erat leo, nec adipiscing eros at.
+</p>
+<?php require __DIR__ . '/templates/footer.php'; ?>
+
---
+++ login.php
@@ -0,0 +1,8 @@
+<?php require __DIR__ . '/templates/header.php'; ?>
+<form action="" method="post">
+    <label for="email">Email</label>
+    <input type="email" name="email" id="email">
+    <button type="submit">Connexion</button>
+</form>
+<?php require __DIR__ . '/templates/footer.php'; ?>
+
